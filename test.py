4ab3c6707929b0b2b7d5d5ec036b086cf081644d
Allen <836414757@qq.com>
2018-12-26 17:08:06 +0800

    pygame 模块使用练习

--- test.py
+++ test.py
@@ -9,7 +9,20 @@ print("英雄的宽高 %d*%d" % (hero_rect.width, hero_rect.height))
 print("英雄的size %d*%d" % hero_rect.size)
 
 # 创建游戏窗口
-screen = pygame.display.set_mode((480, 800))
+screen = pygame.display.set_mode((480, 700))
+# 加载游戏背景图
+bg = pygame.image.load("./images/background.png")
+# 绘制游戏背景图
+screen.blit(bg, (0, 0))
+
+hero = pygame.image.load("./images/me1.png")
+screen.blit(hero, (200, 400))
+
+# 刷新游戏窗口
+pygame.display.update()
+
+
+
 
 # 游戏循环
 while True:
