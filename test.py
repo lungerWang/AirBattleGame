b3939260aa9f674f6c3e8fe70b5df5bb7d99ef92
Allen <836414757@qq.com>
2018-12-27 13:43:26 +0800

    关闭游戏

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 # 创建英雄矩形
-hero_rect = pygame.Rect(500, 600, 50, 60)
+hero_rect = pygame.Rect(200, 600, 102, 126)
 print("英雄的xy坐标 (%d, %d)" % (hero_rect.x, hero_rect.y))
 print("英雄的宽高 %d*%d" % (hero_rect.width, hero_rect.height))
 print("英雄的size %d*%d" % hero_rect.size)
@@ -13,19 +13,36 @@ screen = pygame.display.set_mode((480, 700))
 # 加载游戏背景图
 bg = pygame.image.load("./images/background.png")
 # 绘制游戏背景图
-screen.blit(bg, (0, 0))
+# screen.blit(bg, (0, 0))
 
 hero = pygame.image.load("./images/me1.png")
-screen.blit(hero, (200, 400))
+# screen.blit(hero, (200, 400))
 
 # 刷新游戏窗口
-pygame.display.update()
+# pygame.display.update()
 
 
-
+# 游戏时钟对象
+clock = pygame.time.Clock()
 
 # 游戏循环
 while True:
+    clock.tick(60)
+    # 获取用户事件并打印
+    event_list = pygame.event.get()
+    for event in event_list:
+        # 实现关闭游戏功能
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    hero_rect.y -= 1
+    if hero_rect.y <= -126:
+        hero_rect.y = 700
+        
+    screen.blit(bg, (0, 0))
+    screen.blit(hero, hero_rect)
+    pygame.display.update()
     pass
 
 pygame.quit()
